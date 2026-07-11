a04bf840b7d5f2f47bfff6dafadb933a652734ab
Nasser934 <lah03017@gmail.com>
2026-07-11 10:49:41 +0300

    Simplify preprocessing so duration fixes are handled after compilation

--- shortcut-v60/preprocess.py
+++ shortcut-v60/preprocess.py
@@ -73,30 +73,6 @@ p = Path('shortcut-v60/prayer-main-v60.cherri')
 s = p.read_text(encoding='utf-8')
 s = re.sub(r"comment\('''.*?'''\)\s*", "", s, flags=re.S)
 s = re.sub(r'getFile\(("PrayerAlarm/[^\"]+"), false\)', r'getFile(\1, nil, false)', s)
-s = s.replace('@tomorrow = adjustDate(@now, "Add", qty(1, "days"))', '@oneDay = qty(1, "days")\n@tomorrow = adjustDate(@now, "Add", @oneDay)')
-s = s.replace('@targetTime = adjustDate(@targetTime, "Add", qty(@offset, "min"))', '@offsetDuration = qty(@offset, "min")\n            @targetTime = adjustDate(@targetTime, "Add", @offsetDuration)')
-replacements = {
-    '@method = getMatchGroup(@methodMatch, 1)': '@methodRaw = getMatchGroup(@methodMatch, 1)\n@method = getText(@methodRaw)',
-    '@locationMode = getMatchGroup(@locationModeMatch, 1)': '@locationModeRaw = getMatchGroup(@locationModeMatch, 1)\n@locationMode = getText(@locationModeRaw)',
-    '@latitude = getMatchGroup(@latMatch, 1)': '@latitudeRaw = getMatchGroup(@latMatch, 1)\n    @latitude = getText(@latitudeRaw)',
-    '@longitude = getMatchGroup(@lonMatch, 1)': '@longitudeRaw = getMatchGroup(@lonMatch, 1)\n    @longitude = getText(@longitudeRaw)',
-    '@cacheDate = getValue(@cacheData, "data.date.gregorian.date")': '@cacheDateRaw = getValue(@cacheData, "data.date.gregorian.date")\n        @cacheDate = getText(@cacheDateRaw)',
-    '@weekday = getValue(@apiData, "data.date.gregorian.weekday.en")': '@weekdayRaw = getValue(@apiData, "data.date.gregorian.weekday.en")\n@weekday = getText(@weekdayRaw)',
-    '@hijriMonth = getValue(@apiData, "data.date.hijri.month.number")': '@hijriMonthRaw = getValue(@apiData, "data.date.hijri.month.number")\n@hijriMonth = getText(@hijriMonthRaw)',
-    '@successEnabled = getMatchGroup(@successMatch, 1)': '@successEnabledRaw = getMatchGroup(@successMatch, 1)\n@successEnabled = getText(@successEnabledRaw)',
-}
-for old, new in replacements.items():
-    s = s.replace(old, new)
-s = s.replace('''for line in @settingsLines {
-    if @line contains "|" {''', '''for line in @settingsLines {
-    @lineText = getText(@line)
-    if @lineText contains "|" {''')
-s = s.replace('@parts = splitText(@line, "|")', '@parts = splitText(@lineText, "|")')
-s = s.replace('@key = getListItem(@parts, 1)', '@keyRaw = getListItem(@parts, 1)\n        @key = getText(@keyRaw)')
-s = s.replace('@mode = getListItem(@parts, 2)', '@modeRaw = getListItem(@parts, 2)\n        @mode = getText(@modeRaw)')
-s = s.replace('@offset = getListItem(@parts, 3)', '@offsetRaw = getListItem(@parts, 3)\n        @offset = getText(@offsetRaw)')
-s = s.replace('@offset = getListItem(@parts, 4)', '@offsetRaw = getListItem(@parts, 4)\n                @offset = getText(@offsetRaw)')
-s = s.replace('@qiyamSource = getListItem(@parts, 4)', '@qiyamSourceRaw = getListItem(@parts, 4)\n            @qiyamSource = getText(@qiyamSourceRaw)')
 p.write_text(s, encoding='utf-8')
 
 settings_final = Path('shortcut-v60/prayer-settings-v60.cherri').read_text(encoding='utf-8')
@@ -104,5 +80,4 @@ main_final = Path('shortcut-v60/prayer-main-v60.cherri').read_text(encoding='utf
 assert 'chooseFromList(list(' not in settings_final
 assert '@methodName ==' not in settings_final
 assert '@modeChoice ==' not in settings_final
-assert 'adjustDate(@now, "Add", qty' not in main_final
 assert 'getFile("PrayerAlarm/settings.txt", false)' not in main_final
